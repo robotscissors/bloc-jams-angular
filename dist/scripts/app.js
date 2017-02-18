ba78a0c52c7e4702dec912ecc339f9fe74882017
robotscissors <cjbazin@gmail.com>
2017-02-17 18:20:54 -0800

    Revision to Assignment 3

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -11,7 +11,16 @@
       .state('landing', {
       url: '/',
       templateUrl: '/templates/landing.html'
+    })
+      .state('album', {
+      url: '/album',
+      templateUrl: '/templates/album.html'
+    })
+      .state('collection', {
+      url: '/collection',
+      templateUrl: '/templates/collection.html'
     });
+
   }
 
   angular.module('blocJams', ['ui.router']);
